32dd94dea71ec6cd0f0f292db1cd2ab1606f8015
Irene Brignoli <ire.brignoli@gmail.com>
2023-03-31 18:00:05 +0200

    provo a importare api

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,16 @@ export default {
       store,
     };
   },
+  created() {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/search/movie?api_key=38108238e55a723ea029b5154ff23a5b&query=constantine"
+      )
+      .then((response) => {
+        this.store.movieList = response.data.results;
+        console.log(response.data.results);
+      });
+  },
 };
 </script>
 
